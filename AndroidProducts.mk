037397afdac85e61a5acf331a9b8e6accf332693
PGYT PROJECT <PROJECT@KERNEL>
2024-09-06 09:47:27 +0700

    Recovery Tree New Empty

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_LH6n.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_LH6n-user \
+    twrp_LH6n-userdebug \
+    twrp_LH6n-eng
